07261728b2b77289b87e64aa34e764e8e052cfbb
Daag <aadamico@gmail.com>
2019-04-14 16:49:03 -0400

    Picking up where left off

---
+++ src/api/dal/graphql/graphqlDal.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let { buildSchema } = require('graphql');
+let data = require('../../../db/data/mocks/magicitems_mock')();
+
+module.exports = function () {
+    let schema = buildSchema(`
+        type Query {
+            hello: String,
+            magicitems: [MagicItem],
+            categories: [Category]
+        }
+
+        type MagicItem {
+            id: Int!,
+            name: String!,
+            category: Category
+        }
+
+        type Category {
+            id: Int!,
+            name: String!
+        }
+    `);
+
+    let root = {
+        hello: () => 'Hello World!', 
+        magicitems: () => data.magicitems,
+        categories: () => data.categories
+    }
+
+    return {
+        schema: schema,
+        root: root
+    }
+}
